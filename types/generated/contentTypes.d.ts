b2879779378cd1443d9084056b0545e86b9c2df9
vinhlh <vinhlh@hypurx.vn>
2025-10-09 10:46:50 +0700

    add image in news

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -715,6 +715,7 @@ export interface ApiFeatureItemFeatureItem extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
@@ -984,6 +985,7 @@ export interface ApiNewsItemNewsItem extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    image: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
