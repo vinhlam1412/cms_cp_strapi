cff6a7de56076f7b018151be0e4a7b4f6d39520e
vinhlh <vinhlh@hypurx.vn>
2025-10-30 15:52:41 +0700

    fix error

--- config/plugins.ts
+++ config/plugins.ts
@@ -23,18 +23,9 @@ export default ({ env }) => ({
   },
    upload: {
     config: {
+      provider: 'local',
       providerOptions: {
-        localServer: {
-          maxage: 300000
-        },
-      },
-      sizeLimit: 250 * 1024 * 1024, // 256mb in bytes
-      breakpoints: {
-        xlarge: 1920,
-        large: 1000,
-        medium: 750,
-        small: 500,
-        xsmall: 64
+        sizeLimit: 100000,
       },
     },
   },
--- config/server.ts
+++ config/server.ts
@@ -4,9 +4,4 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
-  
-  url: env('PUBLIC_URL', 'https://cms.creativepoint.vn'),
-  proxy: {
-    enabled: true,     
-  },
 });
