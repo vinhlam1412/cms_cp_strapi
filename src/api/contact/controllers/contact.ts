0efd283173a572b50b0e7332d50cd920b84f5082
vinhlh <vinhlh@hypurx.vn>
2025-10-31 12:16:29 +0700

    update config email

--- src/api/contact/controllers/contact.ts
+++ src/api/contact/controllers/contact.ts
@@ -38,8 +38,8 @@ export default {
 
       // Gửi email bằng plugin email
       await strapi.plugin("email").service("email").send({
-        to: 'vinhlam1412@gmail.com',
         from: 'vinhlam1412@gmail.com',
+        to: 'vinhlam1412@gmail.com',
         subject: subject || 'Liên hệ mới từ website CreativePoint',
         html,
         text,
