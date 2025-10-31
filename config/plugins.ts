4e60d9e69536206b46c8b7bb66d35c793936deb3
vinhlh <vinhlh@hypurx.vn>
2025-10-31 11:13:03 +0700

    update config email

--- config/plugins.ts
+++ config/plugins.ts
@@ -5,15 +5,15 @@ export default ({ env }) => ({
    email: {
     config: {
       provider: 'nodemailer', // dùng provider Nodemailer
-      // providerOptions: {
-      //   host: env('SMTP_HOST', 'smtp.example.com'),
-      //   port: env.int('SMTP_PORT', 587),
-      //   secure: env.bool('SMTP_SECURE', false), // true nếu dùng 465
-      //   auth: {
-      //     user: env('SMTP_USERNAME'),
-      //     pass: env('SMTP_PASSWORD'),
-      //   },
-      // },
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.gmail.com'),
+        port: env.int('SMTP_PORT', 587),
+        secure: env.bool('SMTP_SECURE', false), // true nếu dùng 465
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+      },
       settings: {
         defaultFrom: env('EMAIL_FROM', 'no-reply@example.com'),
         defaultReplyTo: env('EMAIL_REPLY_TO', 'no-reply@example.com'),
